d0f918d0902c84771c8459b6163b822eab79d2df
Andrés <andresrf53@educastur.es>
2026-07-03 23:20:57 +0200

    Día 11

--- src/Capitulo3/P2.java
+++ src/Capitulo3/P2.java
@@ -1,19 +1,36 @@
 package Capitulo3;
 
 import java.util.Scanner;
-
+/*crea un programa que pida diez numeros reales por el teclado, los almacene en un array y luego lo recorra para averiguar el maximo y el minimo y mostrarlos
+* por pantalla*/
 public class P2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] numeros = new int[10];
+        double[] numeros = new double[10];
+        double a;
+        double b;
 
         for (int i = 0; i < numeros.length; i++) {
             System.out.print("Introduce un numero: ");
-            numeros[i] = sc.nextInt();
+            numeros[i] = sc.nextDouble();
         }
 
+        //calculamos el número más grande
+        a = numeros[0];
         for (int i = 0; i < numeros.length; i++) {
-
+            if (numeros[i] > a) {
+                a = numeros[i];
+            }
         }
+
+        //calculamos en número más pequeño
+        b = numeros[0];
+        for (int i = 0; i < numeros.length; i++) {
+            if (numeros[i] < b) {
+                b = numeros[i];
+            }
+        }
+
+        System.out.println("El numero mas grande es:" + a + "\nEl numero mas pequeño es: " + b);
     }
 }
