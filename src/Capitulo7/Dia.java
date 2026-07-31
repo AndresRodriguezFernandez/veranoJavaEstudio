f4443f380e55f12cc909b30b2f7d71dcbcce6c4c
Andrés <andresrf53@educastur.es>
2026-07-31 23:24:23 +0200

    Día 38

--- src/Capitulo7/Dia.java
+++ src/Capitulo7/Dia.java
@@ -2,4 +2,5 @@ package Capitulo7;
 
 public class Dia {
     //Clase de confirmacion. Se ha empezado a estudiar el capitulo 7 del libro.  30/07/2026
+    //Se continua el proceso de estudio del capitulo 7 del libro.   31/07/2026
 }
