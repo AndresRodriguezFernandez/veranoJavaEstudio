def133cb386ab619ff7ca7944655fb0e67258cff
Andrés <andresrf53@educastur.es>
2026-08-05 21:08:37 +0200

    Día 43

---
+++ src/Capitulo7/P6/Principal.java
@@ -0,0 +1,8 @@
+package Capitulo7.P6;
+
+public class Principal {
+    public static void main(String[] args) {
+        Raices r1 = new Raices(1, 0, -9);
+        r1.Calcular();
+    }
+}
--- src/Capitulo7/P6/Raices.java
+++ src/Capitulo7/P6/Raices.java
@@ -1,7 +1,9 @@
 package Capitulo7.P6;
 
 public class Raices {
-    private int a, b, c;
+    private int a;
+    private int b;
+    private int c;
 
     public Raices(int a, int b, int c){
         this.a=a;
@@ -10,6 +12,51 @@ public class Raices {
     }
 
     public double getDiscriminante() {
-        return (Math.pow(this.b, 2))-4*this.a*this.c;
+        return ((Math.pow(b, 2)) - (4 * a * c));
+    }
+
+    //devuelve un boolean indicando si tiene dos soluciones
+    //true si las tiene
+    public boolean tieneRaices(double discriminante){
+        boolean res;
+        if (discriminante > 0){
+            res = true;
+        }else {
+            res = false;
+        }
+        return res;
+    }
+
+    //true si tiene una única solución
+    public boolean tieneRaiz(double discriminante) {
+        boolean res;
+        if (discriminante == 0){
+            res = true;
+        }else {
+            res = false;
+        }
+        return res;
+    }
+
+    public void obtenerRaices(double discriminante){
+        double res1 = ((-this.b)+Math.sqrt(discriminante))/(2*this.a);
+        double res2 = ((-this.b)-Math.sqrt(discriminante))/(2*this.a);
+        System.out.println("Solución 1: " + res1 + "\nSolución 2: " + res2);
+    }
+
+    public void obtenerRaiz(double discriminante){
+        double resUni = ((-this.b)+Math.sqrt(discriminante))/(2*this.a);
+        System.out.println("La solución única es: " + resUni);
+    }
+
+    public void Calcular() {
+        double discriminante = getDiscriminante();
+        if (tieneRaices(discriminante)) {
+            obtenerRaices(discriminante);
+        } else if (tieneRaiz(discriminante)) {
+            obtenerRaiz(discriminante);
+        }else if (discriminante < 0){
+            System.out.println("La ecuación no tiene solución real.");
+        }
     }
 }
