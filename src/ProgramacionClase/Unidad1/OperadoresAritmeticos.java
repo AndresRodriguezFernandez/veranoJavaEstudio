94fdf04ed7cd8fd9d4e34d2074e8a5e3b0d4963f
Andrés <andresrf53@educastur.es>
2026-08-14 16:25:51 +0200

    Día 52

---
+++ src/ProgramacionClase/Unidad1/OperadoresAritmeticos.java
@@ -0,0 +1,20 @@
+package ProgramacionClase.Unidad1;
+
+public class OperadoresAritmeticos {
+    public static void main(String[] args) {
+        //a
+        double multiplicacion =2.2*1.0;
+        double resultado = multiplicacion + 5.0;
+        System.out.println(resultado);
+
+        //b
+        int impuesto2 = (12/2)+(-8);
+        System.out.println(impuesto2);
+
+        //c
+        int cociente = 16/3;
+        int resto = 16%3;
+        double cociente_decimal = 16/3;
+        System.out.println(cociente + " " + cociente_decimal);
+    }
+}
